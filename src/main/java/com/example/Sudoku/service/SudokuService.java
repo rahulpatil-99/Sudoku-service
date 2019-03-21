f8307620e1f92bc463e9349b6fc85980d37bc65b
Rahul <in-rahul.patil@in-rahulpatil.local>
2019-03-21 11:46:15 +0530

    validate no duplicate element is present in any one of the column

--- src/main/java/com/example/Sudoku/service/SudokuService.java
+++ src/main/java/com/example/Sudoku/service/SudokuService.java
@@ -3,15 +3,38 @@ package com.example.Sudoku.service;
 import com.example.Sudoku.domain.Grid;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
+import java.util.ArrayList;
 
 @Service
 public class SudokuService {
 
     public Boolean validate(Grid grid) {
+        return DoesHaveUniqueElements(grid) && DoesHaveUniqueElements(transpose(grid));
+    }
+
+    public boolean DoesHaveUniqueElements(Grid grid) {
         return grid.getDetails()
                 .stream()
-                .anyMatch(s-> s.size() == new HashSet<>(s).size());
+                .anyMatch(s-> s.size() == s.stream().distinct().toArray().length);
+    }
 
+
+    public Grid transpose(Grid grid) {
+        ArrayList<ArrayList<String>> rows = grid.getDetails();
+        ArrayList<ArrayList<String>> columns = createColumnsOfSize(rows.size());
+        rows.forEach(row -> {
+            for (int pos = 0; pos < row.size(); pos++) {
+                columns.get(pos).add(row.get(pos));
+            }
+        });
+        return new Grid(columns);
+    }
+
+    private ArrayList<ArrayList<String>> createColumnsOfSize(int size) {
+        ArrayList<ArrayList<String>> columns = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            columns.add(new ArrayList<>());
+        }
+        return columns;
     }
 }
--- src/test/java/com/example/Sudoku/service/SudokuServiceTest.java
+++ src/test/java/com/example/Sudoku/service/SudokuServiceTest.java
@@ -2,44 +2,62 @@ package com.example.Sudoku.service;
 
 import com.example.Sudoku.domain.Grid;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class SudokuServiceTest {
 
     SudokuService sudokuService;
-    ArrayList<ArrayList<String>> row;
+    ArrayList<ArrayList<String>> rows;
 
     @Before
     public void setUp() {
         sudokuService = new SudokuService();
-        row = new ArrayList<>();
+        rows = new ArrayList<>();
     }
 
     @Test
     public void shouldReturnTrueWhenNoNumberIsRepeated() {
-        ArrayList<String> list = new ArrayList<>(Arrays.asList("1", "2", "3"));
-        row.add(list);
-        row.add(list);
-        row.add(list);
-        Boolean expectedValidation = sudokuService.validate(new Grid(row));
-        assertTrue(expectedValidation);
+        ArrayList<String> row1 = new ArrayList<>(Arrays.asList("1", "2", "3"));
+        ArrayList<String> row2 = new ArrayList<>(Arrays.asList("2", "3", "4"));
+        ArrayList<String> row3 = new ArrayList<>(Arrays.asList("4", "1", "2"));
+        rows.add(row1);
+        rows.add(row2);
+        rows.add(row3);
+        assertTrue(sudokuService.validate(new Grid(rows)));
     }
 
     @Test
     public void shouldReturnFalseWhenNumberIsRepeated() {
-        ArrayList<String> list = new ArrayList<>(Arrays.asList("1", "1", "3"));
-        row.add(list);
-        Boolean expectedValidation = sudokuService.validate(new Grid(row));
-        assertFalse(expectedValidation);
+        ArrayList<String> row = new ArrayList<>(Arrays.asList("1", "1", "3"));
+        rows.add(row);
+        assertFalse(sudokuService.validate(new Grid(rows)));
     }
 
+    @Test
+    public void shouldReturnFalseWhenColumnHaveRepeatedNumber() {
+        ArrayList<String> row = new ArrayList<>(Arrays.asList("1", "2"));
+        rows.add(row);
+        rows.add(row);
+        assertFalse(sudokuService.validate(new Grid(rows)));
+    }
 
+    @Test
+    public void shouldTransposeGivenRowsIntoColumns() {
+        ArrayList<String> row1 = new ArrayList<>(Arrays.asList("1", "2"));
+        ArrayList<String> row2 = new ArrayList<>(Arrays.asList("3", "4"));
+        rows.add(row1);
+        rows.add(row2);
+        ArrayList<String> col1 = new ArrayList<>(Arrays.asList("1", "3"));
+        ArrayList<String> col2 = new ArrayList<>(Arrays.asList("2", "4"));
+        ArrayList<ArrayList<String>> columns = new ArrayList<>();
+        columns.add(col1);
+        columns.add(col2);
+        ArrayList<ArrayList<String>> expected = sudokuService.transpose(new Grid(rows)).getDetails();
+        assertEquals(columns, expected);
+    }
 }
